c64aec0a01d0b36f5f81bf464330adff9aa40e00
Rodrigo Faustino <ev.rodrigo.eterno@gmail.com>
2019-10-08 21:49:22 -0300

    new

--- tradingview/lib/main.dart
+++ tradingview/lib/main.dart
@@ -24,15 +24,16 @@ const request ="https://api.hgbrasil.com/finance?format=json&key=c8572271";
 class _HomeState extends State<Home> {
   final realController = TextEditingController();
   final dollarController = TextEditingController();
-  final euroController =TextEditingController();
+  final btcController =TextEditingController();
   final fmt = NumberFormat("#,##0.00", "pt_BR");
 
-  double dollar, euro;
+  double dollar, btc;
   Future<Map> dados;
 
   @override
   void initState() {
     dados = _getData();
+    //print(dados['results']['currencies']['USD']['buy']);
     super.initState();
   }
 
@@ -41,14 +42,58 @@ class _HomeState extends State<Home> {
     return json.decode(response.body);
   }
 
+  Widget buildTextField(String label,String prefixo,
+      TextEditingController controller, Function function){
+    return TextField(
+    controller: controller,
+    decoration: InputDecoration(
+      labelText: label,
+      prefixText: prefixo,
+      border: OutlineInputBorder(),
+      labelStyle: TextStyle(color: Colors.amber)
+    ),
+    style: TextStyle(color: Colors.amber, fontSize: 25),
+    onChanged: function,
+    keyboardType: TextInputType.numberWithOptions(decimal: true),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder<Map>(
-      future: dados,
-      builder: (context,snapshot){
-        return null;
+    return Scaffold(
+      body: FutureBuilder<Map>(
+        future: dados,
+        builder: (context,snapshot)
+      {
+        switch (snapshot.connectionState) {
+          case ConnectionState.none:
+          case ConnectionState.waiting:
+            return Center(child: Text('Carregando dados...'));
+          default:
+            if (snapshot.hasError) {
+              return Center(child: Text('Erro ao carregar Dados:('));
+            } else {
+              dollar = snapshot.data['results']['currencies']['USD']['buy'];
+              btc = snapshot.data['results']['currencies']['BTC']['buy'];
+
+              return SingleChildScrollView(
+              padding: EdgeInsets.all(10),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: <Widget>[
+                    Divider(),
+                    buildTextField('Reais', 'R\$', realController, null),
+                    Divider(),
+                    buildTextField('Dólares', 'US\$', dollarController, null),
+                    Divider(),
+                    buildTextField('BTC', '₿', btcController, null)
+                  ],
+                ),
+              );
+            }
+        }
       },
+      ),
     );
   }
 }
